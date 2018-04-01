1a43afcef51f1416da5cac16268c0eafb28f4fad
RadostinaY <ryordanova8@gmail.com>
2018-04-01 23:12:46 +0300

    Homework 1

--- Problem_1.R
+++ Problem_1.R
@@ -1,1 +1,25 @@
+#a
+x <- c(4, 1, 1, 4)
 
+#b
+y <- c(1, 4)
+
+#c
+diff <- c(x - y) # vector y recycles until it reaches the same length as x
+
+#d
+s <- c(x, y)
+
+#e
+rep(s, 10)
+length(rep(s, 10))
+
+#f
+rep(s, each = 3)
+
+#g
+seq(7, 21)
+rep(7:21)
+
+#h
+length(seq(7, 21))
--- Problem_2.R
+++ Problem_2.R
@@ -1,1 +1,30 @@
+#a
+xmin <- c(23.0, 20.5, 28.2, 20.3, 22.4, 17.2, 18.2)
+xmax <- c(25.0, 22.8, 31.2, 27.3, 28.4, 20.2, 24.1)
 
+#b
+d <- c(xmax - xmin)
+
+#c
+averageMin <- mean(xmin)
+averageMax <- mean(xmax)
+
+#d
+belowAvg <- xmin[xmin < 21.4]
+
+#e
+aboveAvg <- xmin[xmin > 21.4]
+
+#f
+dayNames <- c('Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun')
+names(xmin) <- dayNames
+names(xmax) <- dayNames
+
+#g + h
+temperatures <- data.frame(xmin = xmin, xmax = xmax, xminFahrenheit = 9 / 5 * xmin + 32)
+
+#i
+temperaturesFahrenheit <- data.frame(xmin = 9 / 5 * xmin + 32, xmax = 9 / 5 * xmax + 32)
+
+#j
+temperaturesFirstFiveDays <- data.frame(xmin = xmin[c('Mon', 'Tue', 'Wed', 'Thu', 'Fri')], xmax = xmax[c('Mon', 'Tue', 'Wed', 'Thu', 'Fri')])
